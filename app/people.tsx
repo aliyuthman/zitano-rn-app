fae5de817d09b637b431b1b54b6d96865d475585
Aliyu Usman <aliyu@Aliyus-MacBook-Pro.local>
2025-07-04 15:00:39 -0700

    Initial commit

---
+++ app/people.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const people = () => {
+  return (
+    <View>
+      <Text>people</Text>
+    </View>
+  )
+}
+
+export default people
+
+const styles = StyleSheet.create({})
