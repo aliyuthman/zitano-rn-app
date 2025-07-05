a382ad395ea0e781740222cf9e2ad6c47f4051c2
Aliyu Usman <aliyu@Aliyus-MacBook-Pro.local>
2025-07-05 03:25:36 -0700

    Update app layout and index, add constants directory

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,77 @@
-import { Stack, Tabs } from "expo-router";
+import { colors } from "@/constants/color";
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
 
 export default function RootLayout() {
-  return <Tabs />;
+  return (
+    <Tabs
+      screenOptions={{
+        headerShadowVisible: false,
+        headerStyle: {
+          backgroundColor: colors.background,
+        },
+        headerTintColor: colors.text,
+        headerTitleStyle: {
+          fontFamily: "Poppins-Bold",
+          fontWeight: "bold",
+        },
+        tabBarStyle: {
+          backgroundColor: colors.background,
+          borderTopColor: colors.inactive,
+          borderTopWidth: 1,
+          paddingTop: 10,
+          paddingBottom: 10,
+        },
+        tabBarActiveTintColor: colors.text,
+        tabBarInactiveTintColor: colors.inactive,
+        tabBarLabelStyle: {
+          fontFamily: "Poppins-Bold",
+          fontWeight: "bold",
+        },
+        tabBarIconStyle: {
+          marginBottom: 10,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          href: null,
+        }}
+      />
+
+      <Tabs.Screen
+        name="films"
+        options={{
+          title: "All Movies",
+          tabBarLabel: "Films",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="film" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="people"
+        options={{
+          title: "All People",
+          tabBarLabel: "People",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="favorites"
+        options={{
+          title: "My Favorites",
+          tabBarLabel: "Favorites",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="heart" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
---
+++ constants/color.ts
@@ -0,0 +1,8 @@
+export const colors = {
+  background: "#000000",
+  text: "#FFFFFF",
+  inactive: "#808080",
+  itemBackground: "#121212",
+  containerBackground: "#121212",
+  
+};
